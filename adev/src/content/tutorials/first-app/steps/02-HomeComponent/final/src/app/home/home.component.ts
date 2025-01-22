83db40c0e592b10818556dcfcd65a0a600667471
dancer1325 <alfredotic0809@gmail.com>
2025-01-22 21:55:39 +0100

    doc(adev.tutorials.firstApp.steps.2homeComponent): clean notes

---
+++ adev/src/content/tutorials/first-app/steps/02-HomeComponent/final/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+})
+export class HomeComponent {}
