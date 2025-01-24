68ebf9ac88aecede65dbe91dc1667cd806387189
dancer1325 <alfredotic0809@gmail.com>
2025-01-24 08:27:20 +0100

    doc(adev.tutorials.firstApp.steps.4interfaces): clean notes and add example

---
+++ adev/src/content/tutorials/first-app/steps/04-interfaces/final/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  template: `
+    <p>housing-location works!</p>
+  `,
+  standalone: true,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {}
