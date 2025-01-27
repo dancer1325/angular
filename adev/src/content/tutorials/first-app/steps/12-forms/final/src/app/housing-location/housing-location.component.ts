32f8d217e1dab12fba51f62956c827e9407fc50a
dancer1325 <alfredotic0809@gmail.com>
2025-01-27 13:50:53 +0100

    doc(tutorials.firstApp.steps.12forms): clean notes and add examples

---
+++ adev/src/content/tutorials/first-app/steps/12-forms/final/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+import {HousingLocation} from '../housinglocation';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [RouterLink],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+
+      <!-- Angular router -- thanks to routerLink, can create -- dynamic links | application -->
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  standalone: true,
+  styleUrl: './housing-location.component.css',
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
